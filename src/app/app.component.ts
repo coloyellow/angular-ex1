b12b38165917764f3fcfde0e43842d3650e78f6c
黃冠綸 <localyellow@MyMacBookAir.local>
2021-03-30 20:21:09 +0800

    writeAnswer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,20 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+  count = 0;
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
     }
+  }
+  countWord(wordLenght : string){
+    this.count = wordLenght.length;
+
+  }
+  cleanWord(input : HTMLInputElement){
+    this.count = 0;
+    input.value = "";
   }
 }
